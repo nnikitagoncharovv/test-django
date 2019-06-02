f1363d5fc0cc8b6b472a394dab12214b9d7f716f
Nikita Goncharov <nnikitagoncharovv@gmail.com>
2019-06-02 16:30:07 +0300

    new project django

---
+++ mysite/mainApp/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'mainApp/homePage.html')
+
+def contact(request):
+    return render(request, 'mainApp/basic.html', {'values': ['Если у вас остались вопросы, задавайте их мне по телефону', '(068) 068-68-68', 'example@mail.ru']})
